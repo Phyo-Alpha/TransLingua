eb574f8e841755117daf097294102702801dc530
Phyo-Alpha <77912443+Phyo-Alpha@users.noreply.github.com>
2025-04-25 15:21:39 +0800

    change the port env for render

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     await app.listen(
-        process.env.TRANSLATE_API_PORT ?? 3000,
+        process.env.PORT || 4000,
         process.env.TRANSLATE_API_HOST_NAME!
     );
 }
